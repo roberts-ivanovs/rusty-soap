e93e9c53f7a2127307492e48ac26c4b53fa3871a
Etsetro <ronalds.palacis@outlook.com>
2021-01-15 21:31:34 +0200

    finished ns file

---
+++ src/ns.rs
@@ -0,0 +1,25 @@
+pub static SOAP_11: &str = "http://schemas.xmlsoap.org/wsdl/soap/";
+pub static SOAP_12: &str = "http://schemas.xmlsoap.org/wsdl/soap12/";
+pub static SOAP_ENV_11: &str = "http://schemas.xmlsoap.org/soap/envelope/";
+pub static SOAP_ENV_12: &str = "http://www.w3.org/2003/05/soap-envelope";
+
+pub static XSI: &str = "http://www.w3.org/2001/XMLSchema-instance";
+pub static XSD: &str = "http://www.w3.org/2001/XMLSchema";
+
+pub static WSDL: &str = "http://schemas.xmlsoap.org/wsdl/";
+pub static HTTP: &str = "http://schemas.xmlsoap.org/wsdl/http/";
+pub static MIME: &str = "http://schemas.xmlsoap.org/wsdl/mime/";
+
+pub static WSA: &str = "http://www.w3.org/2005/08/addressing";
+
+
+pub static DS: &str = "http://www.w3.org/2000/09/xmldsig#";
+pub static WSSE: &str = (
+    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+);
+pub static WSU: &str = (
+    "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+);
+
+// NOTE: { XSD: "xsd" }
+pub static NAMESPACE_TO_PREFIX: &str = "xsd";
